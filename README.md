28e87664ed762e8292c766d369e8e82d0e062177
AmethystDevbyFK <connorhartey16@icloud.com>
2024-09-21 16:55:55 +0100

    Updated docs
    a

--- README.md
+++
@@ -1,5 +0,0 @@
-Leaf Essentials Docs
-
-# Tells you all about Leaf.
-## Built with Docusarus
-## Simple, easy to use; user-friendly UI
--- assets/js/acecf23e.516cb77d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/leafdocs/blog","blogTitle":"Blog","authorsListPath":"/leafdocs/blog/authors"}')}}]);
